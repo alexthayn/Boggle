97a83503a414b3c4cf495edd798c0a752272fadf
alexthayn <alexthayn>
2018-11-27 20:13:00 -0700

    Wrote some units tests for navigation and the view models

---
+++ UnitTests/ViewModelTests/MainViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Boggle.Shared.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        public MainViewModel vm;
+
+        [SetUp]
+        public void Setup()
+        {
+            vm = new MainViewModel();            
+        }
+
+        [Test]
+        public void CheckStartupViewIsSetCorrectly()
+        {
+            Assert.AreEqual(vm.MainScreenViewModel, vm.ChildViewModel);   
+        }
+    }
+}
---
+++ UnitTests/ViewModelTests/ViewModelNavigationTests.cs
@@ -0,0 +1,66 @@
+﻿using Boggle.Shared.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.ViewModelTests
+{
+    [TestFixture]
+    public class ViewModelNavigationTests
+    {
+        public MainViewModel mvm;
+        public MainScreenViewModel msvm;
+        public BoggleGameViewModel bgvm;
+
+        [SetUp]
+        public void Setup()
+        {
+            mvm = new MainViewModel();
+            msvm = new MainScreenViewModel(mvm);
+            bgvm = new BoggleGameViewModel(mvm);
+        }
+      
+        // Main Screen Navigation        
+        [Test]
+        public void TestNewGameCommandNavigationFromMainScreen()
+        {
+            msvm.NewGameCommand.Execute(null);
+
+            Assert.AreEqual(mvm.BoggleGameViewModel, mvm.ChildViewModel);
+        }
+
+        [Test]
+        public void TestHighScoreCommandNavigationFromMainScreen()
+        {
+            msvm.HighScoresCommand.Execute(null);
+
+            Assert.AreEqual(mvm.HighScoresViewModel, mvm.ChildViewModel);
+        }
+
+        [Test]
+        public void TestHowToPlayCommandNavigationFromMainScreen()
+        {
+            msvm.HowToPlayCommand.Execute(null);
+
+            Assert.AreEqual(mvm.HowToPlayViewModel, mvm.ChildViewModel);
+        }
+
+        // Game mode navigation
+        [Test]
+        public void TestNewGameCommandNavigationFromGameScreen()
+        {
+            bgvm.NewGameCommand.Execute(null);
+
+            Assert.AreEqual(mvm.BoggleGameViewModel, mvm.ChildViewModel);
+        }
+
+        [Test]
+        public void TestHowToPlayCommandNavigationFromGameScreen()
+        {
+            bgvm.HowToPlayCommand.Execute(null);
+
+            Assert.AreEqual(mvm.HowToPlayViewModel, mvm.ChildViewModel);
+        }
+    }
+}
