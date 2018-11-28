fef7507e053adc81822d73775816f7918de5e6b1
alexthayn <alexthayn>
2018-11-27 18:16:53 -0700

    Finished up menu bars for main screen and game view

--- Boggle.Shared/ViewModels/BoggleGameViewModel.cs
+++ Boggle.Shared/ViewModels/BoggleGameViewModel.cs
@@ -18,5 +18,39 @@ namespace Boggle.Shared.ViewModels
             {
                 mainView.ChildViewModel = mainView.MainScreenViewModel;
             }));
+
+        private RelayCommand _howToPlayCommand;
+        public RelayCommand HowToPlayCommand => _howToPlayCommand ?? (_howToPlayCommand = new RelayCommand(
+            () =>
+            {
+                //Ask user the confirm
+                //Save game and return to game after they are done
+                mainView.ChildViewModel = mainView.HowToPlayViewModel;
+            }));
+
+        private RelayCommand _exitProgramCommand;
+        public RelayCommand ExitProgramCommand => _exitProgramCommand ?? (_exitProgramCommand = new RelayCommand(
+            () =>
+            {
+                //Ask user the confirm
+                Environment.Exit(0);
+            }));
+
+        private RelayCommand _newGameCommand;
+        public RelayCommand NewGameCommand => _newGameCommand ?? (_newGameCommand = new RelayCommand(
+            () =>
+            {
+                //Ask user the confirm
+                mainView.ChildViewModel = mainView.BoggleGameViewModel;
+            }));
+
+        private RelayCommand _endGameCommand;
+        public RelayCommand EndGameCommand => _endGameCommand ?? (_endGameCommand = new RelayCommand(
+            () =>
+            {
+                //Ask user the confirm
+                mainView.ChildViewModel = mainView.MainScreenViewModel;
+                //clean up game here
+            }));
     }
 }
