71d6af3e72a27bede4aaa64101a13899a63fe5e6
alexthayn <alexthayn>
2018-12-12 03:15:03 -0700

    #104 Give the player a visual cue when the guess they entered is invalid or already been entered

---
+++ Boggle.WPF/Converters/BoolToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Boggle.WPF.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            SolidColorBrush retColor = new SolidColorBrush
+            {
+                Color = System.Windows.Media.Color.FromRgb(0, 0, 0)
+            };
+
+            if ((bool)value)
+            {
+                retColor.Color = System.Windows.Media.Color.FromRgb(11, 102, 35);
+            }
+            else
+            {
+                retColor.Color = System.Windows.Media.Color.FromRgb(234, 41, 57);
+            }
+            return retColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
